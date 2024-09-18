869e82e4d611ed3ffe9733ba1fc36db462f3f72b
Vedant Pawar <vedantmpawar2233@gmail.com>
2024-09-18 13:08:45 +0530

    master branch commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
 	public static void main(String args[])
 	{
+		// System.out.println("B1");
+		System.out.println("Master");
 		System.out.println("Normal git bash");
-		System.out.println("Second");
 	}
 }
